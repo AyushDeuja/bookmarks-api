ae9c853e42056a269abb841e0ed93e4c224b84b5
AyushDeuja <ayushdeuja11@gmail.com>
2025-03-31 08:58:52 +0545

    jwt guard created

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
+import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
 
 @Controller('users')
 export class UsersController {
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtGuard)
   @Get('me')
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(@GetUser() user: User) {
+    return user;
   }
+
+  @Patch()
+  editUser() {}
 }
